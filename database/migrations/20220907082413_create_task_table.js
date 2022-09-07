54c848d1b365424932ae68183392499c18bf715b
goldsami <zolotarenkomax@gmail.com>
2022-09-07 18:20:04 +0300

    feat(database): migration for tables creation added

---
+++ database/migrations/20220907082413_create_task_table.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTableIfNotExists('task', table => {
+    table.increments()
+    table.string('name')
+    table.string('description')
+    table.datetime('time')
+    table.enum('status', ['open', 'done']).defaultTo('open')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('task')
+}
