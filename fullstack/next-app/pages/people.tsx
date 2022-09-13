4cec9166391af464dad3dffeb5b8d0cb35b2c61f
goldsami <zolotarenkomax@gmail.com>
2022-09-13 12:56:37 +0300

    feat(fullstack/next): people query added

--- fullstack/next-app/pages/people.tsx
+++ fullstack/next-app/pages/people.tsx
@@ -1,7 +1,22 @@
-export default function People() {
-	return (
-		<>
-			people
-		</>
-	)
+import {User} from "../models";
+import {prisma} from "../prisma";
+
+export default function People({people}: { people: User[] }) {
+  return (
+    <>
+      {people.map((user) => (
+        <div>{user.name}</div>
+      ))}
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  const people = await prisma.user.findMany()
+
+  return {
+    props: {
+      people
+    },
+  };
 }
