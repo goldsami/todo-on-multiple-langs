2f0ad46f012d722e380a898ce9ab91610c0450ff
goldsami <zolotarenkomax@gmail.com>
2022-09-14 20:19:32 +0300

    feat(fullstack/node): navbar added

---
+++ fullstack/next-app/components/navbar.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import {useRouter} from "next/router";
+
+export default function Navbar() {
+  const router = useRouter()
+
+  return (
+    <nav className="navbar is-light" role="navigation" aria-label="main navigation">
+      <div className="navbar-menu">
+        <div className="navbar-start">
+          <Link href="/tasks">
+            <a className={`navbar-item ${router.pathname === '/tasks' && 'is-active'}`}>
+              Tasks
+            </a>
+          </Link>
+
+          <Link href="/people">
+            <a className={`navbar-item ${router.pathname === '/people' && 'is-active'}`}>
+              People
+            </a>
+          </Link>
+        </div>
+      </div>
+    </nav>
+)
+}
