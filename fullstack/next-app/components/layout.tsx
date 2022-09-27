7e66fe0364e9b7b8b31460acf49e79b982857f84
goldsami <zolotarenkomax@gmail.com>
2022-09-27 09:57:04 +0300

    style(fullstack/next): layout changed

--- fullstack/next-app/components/layout.tsx
+++ fullstack/next-app/components/layout.tsx
@@ -1,10 +1,11 @@
 import Navbar from "./navbar";
 
+// @ts-ignore
 export default function Layout({children}) {
   return (
     <>
       <Navbar />
-      <main>
+      <main className='m-5'>
         {children}
       </main>
     </>
