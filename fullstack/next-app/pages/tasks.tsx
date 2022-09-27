d70c29e5d98d6bfcd40f74293ae8d8e1cc312861
goldsami <zolotarenkomax@gmail.com>
2022-09-27 13:42:54 +0300

    feat(fullstack/next): tasks query changed to support user image

--- fullstack/next-app/pages/tasks.tsx
+++ fullstack/next-app/pages/tasks.tsx
@@ -16,8 +16,8 @@ export default function Tasks({ tasks }: { tasks: Task[] }) {
             <div className="is-full level-left level-item is-flex is-flex-direction-column is-align-items-start">
               <div>{task.name}</div>
               <div className="is-size-7 has-text-grey">{task.description}</div>
-              {task.user && <div>
-                <figure className="image is-24x24 mt-1">
+              {task.user?.id != null && <div>
+                <figure className="image is-24x24 mt-1 is-square">
                   <img className='is-rounded' src={task.user.image_url} />
                 </figure>
               </div>}
@@ -31,11 +31,14 @@ export default function Tasks({ tasks }: { tasks: Task[] }) {
 }
 
 export async function getServerSideProps() {
-  const tasks = await prisma.task.findMany()
+  const tasks = (await prisma.$queryRaw`
+    SELECT "task".*, json_build_object('id', "user"."id", 'image_url' ,"user"."image_url") as "user" FROM "task" 
+    LEFT JOIN "user" on "user"."id" = "task"."user_id"
+    GROUP BY "task"."id", "user"."id"`)
 
   return {
     props: {
       tasks: JSON.parse(JSON.stringify(tasks))
-    },
-  };
+    }
+  }
 }
