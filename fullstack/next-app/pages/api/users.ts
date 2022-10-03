3de9bee4427b1597daa1badfc6cfff6fbdd18ca9
goldsami <zolotarenkomax@gmail.com>
2022-10-03 19:00:12 +0300

    feat(fullstack/next): get users api endpoint added

---
+++ fullstack/next-app/pages/api/users.ts
@@ -0,0 +1,14 @@
+import {NextApiRequest, NextApiResponse} from "next";
+import {prisma} from "../../prisma";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+  const {method} = req
+
+  if (method === 'GET') {
+    const users = await prisma.user.findMany()
+    res.status(200).json({users})
+    return
+  }
+
+  res.end('Unknown route')
+}
