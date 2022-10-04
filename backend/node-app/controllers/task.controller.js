804cc57f36e71710e05f4704aa61efebf7d3d304
goldsami <zolotarenkomax@gmail.com>
2022-10-04 12:35:14 +0300

    feat(backend/node): get tasks enpoint finished

--- backend/node-app/controllers/task.controller.js
+++ backend/node-app/controllers/task.controller.js
@@ -1,5 +1,15 @@
-export function taskController(req, res) {
+import {knexClient} from "../knex.js";
+
+
+export async function taskController(req, res) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({data: [{name: 'test'}, {name: 'test2'}]}))
+  const {rows: tasks} = await knexClient.raw(`
+    SELECT "task".*, json_build_object('id', "user"."id", 'image_url' ,"user"."image_url") as "user" FROM "task"
+    LEFT JOIN "user" on "user"."id" = "task"."user_id"
+    WHERE "task"."status" != 'deleted'
+    GROUP BY "task"."id", "user"."id"
+  `)
+
+  res.end(JSON.stringify({data: tasks}))
 }
