4e50f9017f5c9d830a83d795a5b1b4db00a9b8db
goldsami <zolotarenkomax@gmail.com>
2022-10-06 17:26:09 +0300

    feat(frontend/svelte): svelte query added

--- frontend/svelte-app/src/App.svelte
+++ frontend/svelte-app/src/App.svelte
@@ -1,20 +1,25 @@
 <script lang="ts">
   import Navbar from './lib/Navbar.svelte';
   import { Router, Route } from 'svelte-routing';
+  import { QueryClient, QueryClientProvider } from '@sveltestack/svelte-query';
   import Users from './lib/pages/Users.svelte';
   import Tasks from './lib/pages/Tasks.svelte';
+
+  const queryClient = new QueryClient();
 
   export let url = '';
 </script>
 
-<Router {url}>
-  <Navbar />
-  <div class="content">
-    <Route path="/" component={Tasks} />
-    <Route path="tasks" component={Tasks} />
-    <Route path="users" component={Users} />
-  </div>
-</Router>
+<QueryClientProvider client={queryClient}>
+  <Router {url}>
+    <Navbar />
+    <div class="content">
+      <Route path="/" component={Tasks} />
+      <Route path="tasks" component={Tasks} />
+      <Route path="users" component={Users} />
+    </div>
+  </Router>
+</QueryClientProvider>
 
 <style>
 </style>
