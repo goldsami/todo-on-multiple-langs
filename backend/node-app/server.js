12f02ebe124543edefd6e3c0af328efcdd0c6f1b
goldsami <zolotarenkomax@gmail.com>
2022-10-06 17:25:40 +0300

    feat(backend/node): cors headers added

--- backend/node-app/server.js
+++ backend/node-app/server.js
@@ -1,12 +1,17 @@
-import {taskController} from "./controllers/task.controller.js";
+import { taskController } from "./controllers/task.controller.js";
 import * as http from 'http';
-import {userController} from "./controllers/user.controller.js";
+import { userController } from "./controllers/user.controller.js";
 
 const hostname = '127.0.0.1';
 const port = 4000;
 
 const server = http.createServer((req, res) => {
-  const {url} = req
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  const { url } = req
 
   if (url.startsWith('/api/tasks')) {
     taskController(req, res)
