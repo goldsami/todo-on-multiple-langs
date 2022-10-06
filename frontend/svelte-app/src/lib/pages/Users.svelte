0af3d57a5ad412a2afdf8f024c482a64c1aaf5b6
goldsami <zolotarenkomax@gmail.com>
2022-10-06 17:27:47 +0300

    feat(frontend/svelte): users query added

--- frontend/svelte-app/src/lib/pages/Users.svelte
+++ frontend/svelte-app/src/lib/pages/Users.svelte
@@ -1,1 +1,18 @@
-<div>people</div>
+<script lang="ts">
+  import { useQuery } from '@sveltestack/svelte-query';
+  import axios from 'axios';
+
+  const queryResult = useQuery('users', () =>
+    axios.get('http://localhost:4000/api/users')
+  );
+</script>
+
+{#if $queryResult.isLoading}
+  <span>Loading...</span>
+{:else if $queryResult.error}
+  <span>An error has occurred: {$queryResult.error.message}</span>
+{:else}
+  {#each $queryResult.data.data as user}
+    <div>{user.name}</div>
+  {/each}
+{/if}
