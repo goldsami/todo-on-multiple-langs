655affadb264e48e449864a9debd027494b2e922
goldsami <zolotarenkomax@gmail.com>
2022-10-07 12:32:33 +0300

    feat(frontend/svelte): tasks query added

--- frontend/svelte-app/src/lib/pages/Tasks.svelte
+++ frontend/svelte-app/src/lib/pages/Tasks.svelte
@@ -1,1 +1,18 @@
-<div>tasks</div>
+<script lang="ts">
+  import { useQuery } from '@sveltestack/svelte-query';
+  import axios from 'axios';
+
+  const queryResult = useQuery('tasks', () =>
+    axios.get('http://localhost:4000/api/tasks')
+  );
+</script>
+
+{#if $queryResult.isLoading}
+    <span>Loading...</span>
+{:else if $queryResult.error}
+    <span>An error has occurred: {$queryResult.error.message}</span>
+{:else}
+    {#each $queryResult.data.data as task}
+        <div>{task.name}</div>
+    {/each}
+{/if}
