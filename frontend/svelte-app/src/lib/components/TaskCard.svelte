6e5d0ed53cf0f5e748f57ac4c187540f22c37d2c
goldsami <zolotarenkomax@gmail.com>
2022-10-14 17:27:18 +0300

    fix(frontend/svelte): task status toggle fixed

--- frontend/svelte-app/src/lib/components/TaskCard.svelte
+++ frontend/svelte-app/src/lib/components/TaskCard.svelte
@@ -1,40 +1,51 @@
 <script lang="ts">
-  import type {Task} from "../models";
-  import {createEventDispatcher} from "svelte";
-  import {Card, CardBody, CardFooter, CardHeader, CardText, CardTitle, Icon, Image} from "sveltestrap";
+    import type { Task } from '../models';
+    import { createEventDispatcher } from 'svelte';
+    import {
+        Card,
+        CardBody,
+        CardFooter,
+        CardHeader,
+        CardText,
+        CardTitle,
+        Icon,
+    } from 'sveltestrap';
 
-  export let task: Task
+    export let task: Task;
 
-  const dispatch = createEventDispatcher()
+    const dispatch = createEventDispatcher();
 
-  function deleteTask() {
-    dispatch('deleteTask', task.id)
-  }
+    function deleteTask() {
+        dispatch('deleteTask', task.id);
+    }
 
-  function onClick() {
-    dispatch('onClick', task)
-  }
+    function onClick() {
+        dispatch('onClick', task);
+    }
 
-  function updateStatus() {
-    if (task.status === 'open') dispatch('updateStatus', 'done')
-    if (task.status === 'done') dispatch('updateStatus', 'opened')
-  }
+    function updateStatus() {
+        if (task.status === 'open') dispatch('updateStatus', 'done');
+        if (task.status === 'done') dispatch('updateStatus', 'open');
+    }
 </script>
 
 <Card class="m-4">
     <CardHeader class="d-inline-flex justify-content-between">
         <div class="d-inline-flex">
-            <input class="form-check-input me-2"
-                   on:click={updateStatus}
-                   type="checkbox" checked={task.status === 'done'}
-                   aria-label="Checkbox for following text input">
+            <input
+                class="form-check-input me-2"
+                on:click={updateStatus}
+                type="checkbox"
+                checked={task.status === 'done'}
+                aria-label="Checkbox for following text input"
+            />
             <div on:click={onClick}><CardTitle>{task.name}</CardTitle></div>
         </div>
         <div on:click={deleteTask}>
             <Icon name="trash" />
         </div>
     </CardHeader>
-    {#if (task.description)}
+    {#if task.description}
         <CardBody>
             <CardText>
                 {task.description}
@@ -44,7 +55,11 @@
     {#if task.user?.image_url || task.time}
         <CardFooter>
             {#if task.user?.image_url}
-                <img src={task.user.image_url} class="img-thumbnail rounded-circle" alt="...">
+                <img
+                    src={task.user.image_url}
+                    class="img-thumbnail rounded-circle"
+                    alt="..."
+                />
             {/if}
             {#if task.time}
                 <span>{new Date(task.time).toLocaleDateString()}</span>
