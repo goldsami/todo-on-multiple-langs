b7857fc9179b7d2747a69708c982bc970d7f9c16
goldsami <zolotarenkomax@gmail.com>
2022-10-14 17:26:25 +0300

    fix(frontend/svelte): home button fixed

--- frontend/svelte-app/src/lib/components/Navbar.svelte
+++ frontend/svelte-app/src/lib/components/Navbar.svelte
@@ -1,47 +1,56 @@
 <script lang="ts">
-  import {link, useLocation, useNavigate} from 'svelte-navigator';
+    import { link, useLocation, useNavigate } from 'svelte-navigator';
 
-  const location = useLocation();
-  const navigate = useNavigate();
+    const location = useLocation();
+    const navigate = useNavigate();
 
-  const redirectIfNeeded = (location: string) => {
-    if (location === '/') navigate('/tasks')
-  }
+    const redirectIfNeeded = (location: string) => {
+        if (location === '/') navigate('/tasks');
+    };
 
-  $: activeTab = $location.pathname.slice(1)
-  $: redirectIfNeeded($location.pathname)
+    $: activeTab = $location.pathname.slice(1);
+    $: redirectIfNeeded($location.pathname);
 
-  let expandNavbar = false;
+    let expandNavbar = false;
 </script>
 
 <nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/frontend/svelte-app/public">TODO app</a>
+        <a class="navbar-brand" href="/">TODO app</a>
         <button
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-                class="navbar-toggler"
-                data-bs-target="#navbarSupportedContent"
-                data-bs-toggle="collapse"
-                on:click={() => (expandNavbar = !expandNavbar)}
-                type="button"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+            class="navbar-toggler"
+            data-bs-target="#navbarSupportedContent"
+            data-bs-toggle="collapse"
+            on:click={() => (expandNavbar = !expandNavbar)}
+            type="button"
         >
-            <span class="navbar-toggler-icon"/>
+            <span class="navbar-toggler-icon" />
         </button>
         <div
-                class="collapse navbar-collapse"
-                class:show={expandNavbar}
-                id="navbarSupportedContent"
+            class="collapse navbar-collapse"
+            class:show={expandNavbar}
+            id="navbarSupportedContent"
         >
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a aria-current="page" class="nav-link" class:active={activeTab === 'tasks'} href="/tasks" use:link
-                    >Tasks</a
+                    <a
+                        aria-current="page"
+                        class="nav-link"
+                        class:active={activeTab === 'tasks'}
+                        href="/tasks"
+                        use:link>Tasks</a
                     >
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" class:active={activeTab === 'users'} href="/users" use:link>Users</a>
+                    <a
+                        class="nav-link"
+                        class:active={activeTab === 'users'}
+                        href="/users"
+                        use:link>Users</a
+                    >
                 </li>
             </ul>
         </div>
