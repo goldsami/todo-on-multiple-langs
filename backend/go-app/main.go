2e5a22e0d93665145e74da4a938ddc84c1945a89
goldsami <zolotarenkomax@gmail.com>
2022-10-16 11:58:47 +0300

    feat(backend/go): orm added

--- backend/go-app/main.go
+++ backend/go-app/main.go
@@ -1,23 +1,50 @@
 package main
 
 import (
+	"fmt"
+	"github.com/joho/godotenv"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 type MutateTaskType struct {
 	Name        string
 	Description string
+	UserId      string
+	Time        string
+}
+
+type Task struct {
+	Name        string
+	Description string
+	UserId      string
+	Time        string
 }
 
 func main() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
+	dsn := os.Getenv("CONNECTION_STRING")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		return
+	}
+
 	port := ":4000"
 	router := gin.Default()
 	router.GET("/api/tasks", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
+		var tasks []Task
+		db.Find(&tasks)
+		c.IndentedJSON(http.StatusOK, tasks)
 	})
 	router.POST("/api/tasks", func(c *gin.Context) {
 		var requestBody MutateTaskType
