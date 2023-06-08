3473b5a528cac27e8a5d9ff135323f2d396ab626
Goldsami <zolotarenkomax@gmail.com>
2023-06-08 11:59:42 +0300

    fix(frontend/vanilla-js-app): responsibility removed from nav

--- frontend/vanilla-js-app/src/components/navbar.js
+++ frontend/vanilla-js-app/src/components/navbar.js
@@ -37,14 +37,11 @@ class Navbar extends HTMLElement {
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
           <div class="container-fluid">
             <cc-router-link className="navbar-brand" path="${title.url}" name="${title.name}"></cc-router-link>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-              <ul class="navbar-nav">
+            <div id="navbarNav">
+              <ul class="navbar-nav flex-row">
                 ${links.map(x => (`
                   <li class="nav-item">
-                    <cc-router-link className="nav-link" path="${x.url}" name="${x.name}"></cc-router-link>
+                    <cc-router-link className="nav-link p-2" path="${x.url}" name="${x.name}"></cc-router-link>
                   </li>
                 `)).join('')}
               </ul>
