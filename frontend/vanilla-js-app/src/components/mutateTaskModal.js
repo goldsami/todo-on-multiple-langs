40d66c7c98d1734a48c762837f7015acce016100
Goldsami <zolotarenkomax@gmail.com>
2023-06-08 17:39:11 +0300

    style(frontend/vanilla-js-app): modal window styled

--- frontend/vanilla-js-app/src/components/mutateTaskModal.js
+++ frontend/vanilla-js-app/src/components/mutateTaskModal.js
@@ -23,20 +23,28 @@ class MutateTaskModal extends HTMLElement {
   getTemplate() {
     return `
       <div class="overlay"></div>
-      <div class="modal-window" id="task-modal" style="display: none;">
-        <h3 id="modal-name"></h3>
-        <form id="task-form">
-          <label for="task-name">Name:</label>
-          <input type="text" id="task-name" required><br>
-          <label for="task-description">Description:</label>
-          <textarea id="task-description"></textarea><br>
-          <label for="task-user">User:</label>
-          <select id="task-user"></select><br>
-          <label for="task-time">Time:</label>
-          <input type="datetime-local" id="task-time" required><br>
-          <button type="submit">Save</button>
-        </form>
-        <button id="cancel-button">Cancel</button>
+      <div class="modal modal-window" id="task-modal" style="display: none;">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 id="modal-name" class="modal-title"></h5>
+          </div>
+          <form id="task-form">
+            <div class="modal-body">
+              <label for="task-name" class="form-label">Name</label>
+              <input type="text" id="task-name" class="form-control" required><br>
+              <label for="task-description" class="form-label">Description</label>
+              <textarea id="task-description" class="form-control"></textarea><br>
+              <label for="task-user" class="form-label">User</label>
+              <select id="task-user" class="form-control form-select"></select><br>
+              <label for="task-time" class="form-label">Time</label>
+              <input type="datetime-local" id="task-time" class="form-control" required><br>
+            </div>
+            <div class="modal-footer">
+              <button id="cancel-button" type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="submit" class="btn btn-primary">Save changes</button>
+            </div>
+          </form>
+        </div>
       </div>
     `
   }
